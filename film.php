320d3a826a6259092f392fe928b95673112f5a38
Isandre47 <38543400+Isandre47@users.noreply.github.com>
2018-09-11 18:00:28 +0200

    Create film.php

---
+++ film.php
@@ -0,0 +1,10 @@
+<?php
+
+$array = [
+    "Les aventuriers de l'arche perdu" => ['Harrison Ford', 'Karen Allen', 'Paul Freeman'],
+    'Indian Jones et le Temple maudit'=> ['Harrison Ford', 'KAte Capshaw', 'Jonathan Ke Quan'],
+    'Indiana Jones et le Royaume du crane de cristal' => ['Harrison Ford', 'Sean Connery', 'Denholm Elliott']
+    ];
+
+foreach ;
+    echo 'Dans le film ', $movie_title, 'les principaux acteurs sont : ', $name_of_actor_1, $name_of_actor_2, $name_of_actor_3;
